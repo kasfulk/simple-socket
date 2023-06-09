1f747ac07a2e1727f5d01d561e55d9663e904c8a
Kasjful Kurniawan <kasful.bpp@gmail.com>
2023-06-09 17:08:08 +0800

    fix: cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173","*"],
+    origin: ["http://localhost:5173","https://vue-websocket-chat.vercel.app/","*"],
     allowedHeaders: ["custom-header"],
     credentials: true
   }
