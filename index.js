84267904b5d540c1ac324525a25d563f6960a419
Kasjful Kurniawan <kasful.bpp@gmail.com>
2023-06-09 15:08:50 +0800

    fix: cors

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const http = require('http');
 const cors = require("cors");
 const server = http.createServer(app);
-const { Server } = require("socket.io")(server, {
+const { Server } = require("socket.io");
+const io = new Server(server, {
   origins: ["*"],
 
   handlePreflightRequest: (req, res) => {
@@ -14,8 +15,7 @@ const { Server } = require("socket.io")(server, {
     });
     res.end();
   }
-});;
-const io = new Server(server);
+});
 
 app.use(cors());
 
